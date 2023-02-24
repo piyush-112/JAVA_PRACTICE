649f58de146afc9a2995307ea5205c8145380256
PIYUSH <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@github.com>
2023-02-24 19:10:27 +0530

    edit = sign

--- src/starboy.java
+++ src/starboy.java
@@ -5,7 +5,7 @@ public class starboy {
         Scanner S= new Scanner(System.in);
         int n = S.nextInt();
         for(int i=0; i<n; i++) {
-            for(int j=0; j<i; j++) {
+            for(int j=0; j<=i; j++) {
                 System.out.print("*");
             }
             System.out.println();
