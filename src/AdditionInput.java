97b12ff41e12e075bd2e6e59ecd7787983060bc6
PIYUSH <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@github.com>
2023-02-26 21:45:17 +0530

    Commit

---
+++ src/AdditionInput.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class AdditionInput {
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sc = new Scanner(System.in);
+
+        a = sc.nextInt();
+        b= sc.nextInt();
+        c=a+b;
+        System.out.println(c);
+
+    }
+}
---
+++ src/SampleExpression.java
@@ -0,0 +1,10 @@
+public class SampleExpression {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c =(a*b) / (a-b);
+
+        System.out.println(c);
+
+    }
+}
